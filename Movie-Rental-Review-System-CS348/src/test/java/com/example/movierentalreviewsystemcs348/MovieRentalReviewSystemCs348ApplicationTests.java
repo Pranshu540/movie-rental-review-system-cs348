8ba0c5505f0d6eec4f2b3746e790e5179080bec7
Nicholas Jiang <61169805+script-jpg@users.noreply.github.com>
2023-05-24 15:26:36 -0400

    Spring init

---
+++ Movie-Rental-Review-System-CS348/src/test/java/com/example/movierentalreviewsystemcs348/MovieRentalReviewSystemCs348ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movierentalreviewsystemcs348;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRentalReviewSystemCs348ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
