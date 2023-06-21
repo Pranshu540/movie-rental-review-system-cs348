c8736ebcf5d88e0650b317f3b56e9f4742ff34a8
Bluesuey <42977553+Bluesuey@users.noreply.github.com>
2023-06-21 19:28:37 -0400

    initial commit for filter

--- connect.py
+++ connect.py
@@ -3,15 +3,51 @@ import mysql.connector
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
-  password="abcde12345"
+  password="password"
 )
 
-print(mydb)
+titleFilter = input("Enter Filter for Title: ")
+while(True):
+  countFilter = input("Enter Filter for Count: ")
+  try:
+    countFilter = int(countFilter)
+    break
+  except:
+    print("Please enter a number.\n")
 
-mycursor = mydb.cursor()
-mycursor.execute("use sys")
-mycursor.execute("SELECT * FROM movie")
-myresult = mycursor.fetchall()
+genreFilter = input("Enter Filter for Genre: ")
+filterParams = []
+cmd = "SELECT * FROM Movie"
+
+if titleFilter: 
+  cmd += " WHERE title LIKE %s"
+  filterParams.append("%" + titleFilter + "%")
+
+if countFilter > 0:
+  if titleFilter:
+    cmd += " AND"
+  else:
+    cmd += " WHERE"
+  cmd += " rental_quantity >= %s"
+  filterParams.append(countFilter)
+
+if genreFilter:
+  if titleFilter or countFilter:
+    cmd += " AND"
+  else:
+    cmd += " WHERE"
+  cmd += "  genre=%s"
+  filterParams.append(genreFilter)
+
+try: 
+  mycursor = mydb.cursor()
+  mycursor.execute("use sys")
+  mycursor.execute(cmd, filterParams)
+
+  myresult = mycursor.fetchall()
+except mysql.connector.Error as err:
+  print("Something went wrong: {}".format(err))
+
 
 for x in myresult:
   print(x)
