94e772fc83bee822b720d9e2e8af9ed1bf09a11c
Pranshu <pranshu540@gmail.com>
2023-06-22 22:05:01 -0400

    Test feature stuff.

---
+++ testfeatures/input/testreview.sql
@@ -0,0 +1,5 @@
+# Print the review table
+SELECT * FROM Review;
+
+# Add a review from George for The Book Thief:
+INSERT INTO Review VALUES(4, 10, '2023-06-22', 4, 'Teaches me how to steal books!')
