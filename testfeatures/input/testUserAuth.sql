40689499638176a3ac8f442e88eda8dc266e38ef
Nicholas Jiang <61169805+script-jpg@users.noreply.github.com>
2023-06-22 23:00:31 -0400

    Add test sql and out

--- testfeatures/input/testUserAuth.sql
+++ testfeatures/input/testUserAuth.sql
@@ -1,2 +1,4 @@
 SELECT EXISTS(SELECT 1 FROM User WHERE username='movielover123' AND password=’Secret48!’);
+SELECT EXISTS(SELECT 1 FROM User WHERE username='userName' AND password=’userPassword’);
 INSERT INTO Users(username, password) VALUES ('userName', 'userPassword')
+SELECT EXISTS(SELECT 1 FROM User WHERE username='userName' AND password=’userPassword’);
