ff62681b3217978de81b2ae774e9cb0720825cbe
Nicholas Jiang <61169805+script-jpg@users.noreply.github.com>
2023-07-25 10:16:09 -0400

    init

---
+++ front-end/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './components/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ front-end/src/app/components/login/login.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+interface User {
+  username: string;
+  password: string;
+}
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+
+  constructor(private router: Router){
+
+    if (sessionStorage.getItem('username') && sessionStorage.getItem('password')) {
+      console.log('Already logged in');
+      this.router.navigate(['home']);
+    }
+  }
+
+  private setSessionData(formValue: any) {
+    sessionStorage.setItem('username', formValue.username);
+    sessionStorage.setItem('password', formValue.password);
+    alert('Login successful');
+    this.router.navigate(['home']);
+  }
+  
+  
+  userList: User[] = [{username: 'nj', password: 'nj'}]; // TODO: replace with API call
+  submit(formValue: any) {
+    console.log(formValue);
+
+    const user = this.userList.find((user) => user.username === formValue.username && user.password === formValue.password);
+    const newUser = formValue.newUser === 'true';
+    if (!user && newUser) {
+      this.userList.push({username: formValue.username, password: formValue.password});
+      this.setSessionData(formValue);
+    } else if (!user && !newUser) {
+      alert('User not found');
+    } else if (user && newUser) {
+      alert('User already exists');
+    } else {
+      this.setSessionData(formValue);
+    }
+  }
+}
+
