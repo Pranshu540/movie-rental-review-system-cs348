94a915a3625df66b1723853447ac9436dfdbcdba
Jeffrey Liu <jeffrey6162@gmail.com>
2023-07-27 17:31:55 -0400

    change rent movie sig to mid

---
+++ backend/README.md
@@ -0,0 +1,11 @@
+# How to deploy the flask app:
+
+In the backend directory, execute in terminal `export FLASK_APP = run.py`.
+
+You can then execute `flask run` to host the app. 
+
+## Routes
+
+### Auth
+- `/auth/signup`
+- `/auth/signin`
--- backend/movie_rental_system/rentals/routes.py
+++ backend/movie_rental_system/rentals/routes.py
@@ -3,11 +3,11 @@ from .models import rent_movie, get_user_rentals
 from . import rentals
 
 
-@rentals.route('/<string:username>/rent_movie_by_name/<string:moviename>', methods=['POST'])
-def rent_movie_route(username, moviename):
+@rentals.route('/<string:username>/rent_movie_by_name/<int:movie_id>', methods=['POST'])
+def rent_movie_route(username, movie_id):
     try:
         mydb = current_app.config['mysql']
-        response = rent_movie(username, moviename, mydb.connection)
+        response = rent_movie(username, movie_id, mydb.connection)
         return jsonify(response), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 400
