a91a4852f06ca0fef67356c1d4e91a71030998b6
Pranshu <pranshu540@gmail.com>
2023-07-27 19:33:45 -0400

    Test files are up-to date.

--- backend/featureTests/testFeaturesProduction/test-production.sql
+++ backend/featureTests/testFeaturesProduction/test-production.sql
@@ -1,23 +1,23 @@
 #=====Test Filters=====
-SELECT * FROM Movie WHERE title LIKE "%The%" AND rental_quantity >= 1 AND genre = 'Drama';
-SELECT * FROM Movie WHERE rental_quantity >= 10 AND genre = "Sci-Fi";
+SELECT * FROM Movie WHERE title LIKE "%The%" AND rental_quantity >= 1 AND genre = 'Drama' LIMIT 10;
+SELECT * FROM Movie WHERE rental_quantity >= 10 AND genre = "Sci-Fi" LIMIT 10;
 
 #=====Test Rentals=====
 SELECT rental_price FROM Movie WHERE mid = 9;
 
 UPDATE Movie SET rental_quantity = rental_quantity - 1 WHERE mid = 9;
 
-UPDATE User SET wallet = wallet - rental_price WHERE uid = 2;
+UPDATE User SET wallet = wallet - 10 WHERE uid = 2;
 
-INSERT INTO Rental (uid, mid, rent_date, due_date, is_active) VALUES (2, 9, date.today(), date.today() + timedelta(weeks=1), 1);
+INSERT INTO Rental (uid, mid, rent_date, due_date, is_active) VALUES (2, 9, '2023-07-27', '2023-08-10', 1);
 
-SELECT * FROM Rental;
-SELECT * FROM User;
-SELECT * FROM Movie;
+SELECT * FROM Rental LIMIT 10;
+SELECT * FROM User LIMIT 10;
+SELECT * FROM Movie LIMIT 10;
 
 #=====Test Review=====
 # Print the review table
-SELECT * FROM Review;
+SELECT * FROM Review LIMIT 10;
 
 # Add a review from George for The Book Thief:
 INSERT INTO Review VALUES(4, 10, '2023-06-22', 4, 'Teaches me how to steal books!');
@@ -39,11 +39,28 @@ SELECT * FROM Review WHERE mid = 10;
 
 #=====Test User Auth=====
 SELECT EXISTS(SELECT 1 FROM User WHERE username='HumbleGoat456' AND password='xKODPhDD');
-SELECT EXISTS(SELECT 1 FROM User WHERE username='userName' AND password=’userPassword’);
+SELECT EXISTS(SELECT 1 FROM User WHERE username='userName' AND password='userPassword');
 INSERT INTO User(username, password) VALUES ('userName', 'userPassword');
-SELECT EXISTS(SELECT 1 FROM User WHERE username='userName' AND password=’userPassword’);
+SELECT EXISTS(SELECT 1 FROM User WHERE username='userName' AND password='userPassword');
 
 #=====Test User Deletion=====
 DELETE FROM User WHERE uid=4;
 
-SELECT * FROM User;
+SELECT * FROM User LIMIT 10;
+
+#=====Test Recommended List Feature=======
+SELECT t2.genre FROM ((SELECT r.uid, r.mid FROM Rental r) as t1
+JOIN (SELECT m.mid, m.genre FROM Movie m) as t2 ON t1.mid = t2.mid)
+WHERE t1.uid = 1
+GROUP BY t2.genre ORDER BY count(t2.genre) DESC LIMIT 1;
+
+CREATE VIEW Recommended AS
+    SELECT mid, title, rental_price, duration
+    FROM Movie
+    WHERE rental_quantity > 0
+      AND genre = 'drama'
+    ORDER BY RAND(100)
+    LIMIT 200;
+
+SELECT * FROM Recommended LIMIT 5;
+
--- backend/featureTests/testFeaturesSample/test-sample.sql
+++ backend/featureTests/testFeaturesSample/test-sample.sql
@@ -1,15 +1,15 @@
 #=====Test Filters=====
 SELECT * FROM Movie WHERE title LIKE "%The%" AND rental_quantity >= 1 AND genre = 'Drama';
-SELECT * FROM Movie WHERE rental_quantity >= 10 AND genre = ‘Sci-Fi’;
+SELECT * FROM Movie WHERE rental_quantity >= 10 AND genre = "Sci-Fi";
 
 #=====Test Rentals=====
 SELECT rental_price FROM Movie WHERE mid = 9;
 
 UPDATE Movie SET rental_quantity = rental_quantity - 1 WHERE mid = 9;
 
-UPDATE User SET wallet = wallet - rental_price WHERE uid = 2;
+UPDATE User SET wallet = wallet - 10 WHERE uid = 2;
 
-INSERT INTO Rental (uid, mid, rent_date, due_date, is_active) VALUES (2, 9, date.today(), date.today() + timedelta(weeks=2), 1);
+INSERT INTO Rental (uid, mid, rent_date, due_date, is_active) VALUES (2, 9, '2023-07-27', '2023-08-10', 1);
 
 SELECT * FROM Rental;
 SELECT * FROM User;
@@ -23,27 +23,41 @@ SELECT * FROM Review;
 INSERT INTO Review VALUES(4, 10, '2023-06-22', 4, 'Teaches me how to steal books!');
 
 # Print the updated review table
-SELECT * FROM Review;
+SELECT * FROM Review WHERE mid = 10;
 
-# Modify the review from Batman on The Amazing Spider-Man:
-UPDATE Review SET review_date = '2023-06-22', rating = 5, comment = 'Across the Spiderverse changed my mind' WHERE uid = 2 AND mid = 3;
+# Modify the review on INSERT MOVIE HERE
+UPDATE Review SET review_date = '2023-06-22', rating = 5, comment = 'This movie was absolutely amazing I wish I could live in it XD' WHERE uid = 4 AND mid = 10;
 
 # Print the updated review table
-SELECT * FROM Review;
+SELECT * FROM Review WHERE mid = 10;
 
 # Remove george's review on the book thief:
 DELETE FROM Review WHERE uid = 4 AND mid = 10;
 
 # Print the updated review table
-SELECT * FROM Review;
+SELECT * FROM Review WHERE mid = 10;
 
 #=====Test User Auth=====
-SELECT EXISTS(SELECT 1 FROM User WHERE username='movielover123' AND password=’Secret48!’);
-SELECT EXISTS(SELECT 1 FROM User WHERE username='userName' AND password=’userPassword’);
+SELECT EXISTS(SELECT 1 FROM User WHERE username='HumbleGoat456' AND password='xKODPhDD');
+SELECT EXISTS(SELECT 1 FROM User WHERE username='userName' AND password='userPassword');
 INSERT INTO User(username, password) VALUES ('userName', 'userPassword');
-SELECT EXISTS(SELECT 1 FROM User WHERE username='userName' AND password=’userPassword’);
+SELECT EXISTS(SELECT 1 FROM User WHERE username='userName' AND password='userPassword');
 
 #=====Test User Deletion=====
 DELETE FROM User WHERE uid=4;
 
 SELECT * FROM User;
+
+
+#=====Test Recommended List Feature=====
+SELECT m.genre FROM Rental r NATURAL JOIN Movie m WHERE r.uid = 1 GROUP BY m.genre ORDER BY count(m.genre) DESC LIMIT 1;
+
+CREATE VIEW Recommended AS
+    SELECT mid, title, rental_price, duration
+    FROM Movie
+    WHERE rental_quantity > 0
+      AND genre = 'drama'
+    ORDER BY RAND(100)
+    LIMIT 200;
+
+SELECT * FROM Recommended LIMIT 5;
