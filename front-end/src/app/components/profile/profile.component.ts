3620e6163214d2a272f485d9c638f4458e171095
Nicholas Jiang <61169805+script-jpg@users.noreply.github.com>
2023-07-27 22:07:01 -0400

    Update profile.component.ts
    Fix reviews not deleting

--- front-end/src/app/components/profile/profile.component.ts
+++ front-end/src/app/components/profile/profile.component.ts
@@ -3,6 +3,8 @@ import { BackendCommunicationService } from 'src/app/services/backend-communicat
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { RentalDataService } from 'src/app/services/rental-data.service';
+import { Review } from '../review/review.component';
+import { MovieService } from 'src/app/services/movie.service';
 
 @Component({
   selector: 'app-profile',
@@ -18,9 +20,24 @@ export class ProfileComponent {
   constructor(
     private _snackBar: MatSnackBar,
     private router: Router,
-    private rentalService: RentalDataService) {}
+    private rentalService: RentalDataService,
+    private movieService: MovieService) {}
 
-  onDeleteUser() {
+    onDeleteUser() {
+
+    this.movieService.movies.forEach((movie: any) => {
+      movie = movie.title;
+      localStorage.removeItem(this.username+" reviewed "+movie)
+      // delete review if it exists
+      const review: string | null = localStorage.getItem(movie!+'-reviews');
+
+      if (review) {
+        let review_data: Review[] = JSON.parse(review);
+
+        review_data = review_data.filter((review: Review) => review.username !== this.username);
+        localStorage.setItem(movie!+'-reviews', JSON.stringify(review_data));
+      }
+    });
 
     // this.username = '';
     let userList: any[] = [];
@@ -30,16 +47,17 @@ export class ProfileComponent {
     userList = userList.filter(user => user.username !== this.username);
     localStorage.setItem('userList', JSON.stringify(userList));
     
-
+    this.rentalService.clearUserRentals();
     this._snackBar.open('User deleted successfully', 'Close', {
       duration: 1000,
     });
 
-    this.rentalService.clearUserRentals();
+    sessionStorage.removeItem('username');
+    sessionStorage.removeItem('password');
     
-    sessionStorage.clear();
     setTimeout(() => {
       this.router.navigate(['login']);
     }, 1000);
+    
   }
 }
